78a033b4fc02444c55d4d32a2285c3ee4b387c68
lekhraj.rokaya <rokayalekhraj@gmail.com>
2021-12-29 21:19:33 +0545

    jwt.helper.js file is created

---
+++ server/lib/helpers/jwt.helper.js
@@ -0,0 +1,31 @@
+const generateJWTToken = () => {
+  try {
+
+  } catch (error) {
+    throw error;
+  }
+}
+
+const verifyToken = () => {
+  try {
+
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+const decodeToken = () => {
+  try {
+
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+module.exports = {
+  generateJWTToken,
+  verifyToken,
+  decodeToken
+}
