c0f1582777e9c94e984de18e653bb5b5efb41320
lekhraj.rokaya <rokayalekhraj@gmail.com>
2021-12-29 21:14:42 +0545

    swagger.definitions.js file is updated

---
+++ server/swagger.definitions.js
@@ -0,0 +1,72 @@
+
+(() => {
+  const adminAuthApi = require('./lib/modules/admin_auth/api_definitions');
+
+  module.exports = {
+    server: {
+      openapi: '3.0.1',
+      info: {
+        title: 'NBIMF Portal',
+        description: 'NBIMF project',
+        version: '1.0.0',
+      },
+      servers: [
+        {
+          url: 'http://127.0.0.1:4001/api/v1/admin',
+        },
+        {
+          url: 'http://127.0.0.1:4001/api/v1/user',
+        }
+      ],
+      tags: [
+        {
+          name: 'Health Check',
+          description: 'Api Health check.'
+        },
+        ...adminAuthApi.server.tags,
+      ],
+      paths: {
+        ...adminAuthApi.server.paths,
+        '/health-check': {
+          get: {
+            tags: ['Health Check'],
+            summary: 'health check api',
+            description: 'Hit his api inorder to ensure api health.',
+            operationId: 'healthCheck',
+            responses: {
+              default: {
+                description: '',
+                content: {
+                  'application/json': {
+                    schema: {
+                      $ref: '#/components/schemas/CommonResponse',
+                    },
+                  },
+                },
+              },
+            }
+          }
+        }
+      },
+      components: {
+        schemas: {
+          CommonResponse: {
+            type: 'object',
+            properties: {
+              message: { type: 'string' },
+              status: { type: 'number' }
+            },
+          },
+          ...adminAuthApi.server.components.schemas
+        },
+        securitySchemes: {
+          api_key: {
+            type: 'apiKey',
+            name: 'Authorization',
+            in: 'header',
+          },
+        },
+      },
+    },
+  };
+})();
