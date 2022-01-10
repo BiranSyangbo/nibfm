24c11ca7de1095c05a3332ec0f55c9755e491c18
lekhraj.rokaya <rokayalekhraj@gmail.com>
2022-01-10 21:36:39 +0545

    login.controller.js file is updated

--- server/lib/modules/admin_auth/controller/login.controller.js
+++ server/lib/modules/admin_auth/controller/login.controller.js
@@ -1,6 +1,33 @@
+const { loginPayloadValidation } = require('../utils/validation.helper')
+const HTTPStatus = require('http-status');
+const { getUserInfoByUsername } = require('../utils/mongo_query.helper');
+
 module.exports = async (req, res, next) => {
   try {
 
+    const { isValid, msg } = loginPayloadValidation(req.body);
+
+    if (isValid) {
+
+      const getUserInfoByUserName = await getUserInfoByUsername(req, req.body.username);
+      if (getUserInfoByUserName && Object.keys(getUserInfoByUserName).length > 0) {
+        return res.status(HTTPStatus.OK).json({
+          status: HTTPStatus.OK,
+          msg: "Login success.",
+          token: "jwt-token"
+        })
+      }
+
+      return res.status(HTTPStatus.BAD_REQUEST).json({
+        status: HTTPStatus.BAD_REQUEST,
+        msg: "Invalid credential provided."
+      })
+    }
+
+    return res.status(HTTPStatus.BAD_REQUEST).json({
+      status: HTTPStatus.BAD_REQUEST,
+      msg: msg
+    })
   } catch (error) {
     return next(error)
   }
