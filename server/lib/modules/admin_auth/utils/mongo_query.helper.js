4409a38341bc3bfd2ac2585d5d966808421de5e2
lekhraj.rokaya <rokayalekhraj@gmail.com>
2022-01-15 01:24:34 +0545

    mongo_query.helper.js file is updated

--- server/lib/modules/admin_auth/utils/mongo_query.helper.js
+++ server/lib/modules/admin_auth/utils/mongo_query.helper.js
@@ -1,15 +1,53 @@
 const getUserInfoByUsername = async (req, username) => {
   try {
-    return Promise.resolve({
-      id: "78689a48-722c-11ec-b80c-0bdaf5c898b2",
-      username: "username",
-      password: "password"
+    return req.db.collection(process.env.ADMIN_COLLECTION).findOne({
+      deleted: false,
+      email: username
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+const insertLoginInfo = async (db, loginInfo) => {
+  try {
+    return db.collection('login-session').insertOne(loginInfo);
+  } catch (error) {
+    throw error;
+  }
+}
+
+const getUserLoginInfo = async (req, userId) => {
+  try {
+    return req.db.collection('login-session').findOne({
+      token: req.authToken,
+      deleted: false,
+      expiry_time: { $gte: Date.now() },
+      user: userId
     })
   } catch (error) {
     throw error;
   }
 }
 
+const deleteUserLoginInfo = async (db, id) => {
+  try {
+    return db.collection('login-session').updateOne({
+      _id: id
+    },
+      {
+        $set: {
+          deleted: true
+        }
+      })
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
-  getUserInfoByUsername
+  getUserInfoByUsername,
+  insertLoginInfo,
+  getUserLoginInfo,
+  deleteUserLoginInfo
 }
