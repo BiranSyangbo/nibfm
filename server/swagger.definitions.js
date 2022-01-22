de3f24e79a25775d731e0531ccc4656d1618d8df
lekhraj.rokaya <rokayalekhraj@gmail.com>
2022-01-22 15:08:48 +0545

    swagger.definitions.js file is updated

--- server/swagger.definitions.js
+++ server/swagger.definitions.js
@@ -23,10 +23,46 @@
           name: 'Health Check',
           description: 'Api Health check.'
         },
+        {
+          name: 'Static files',
+          description: 'Static files'
+        },
         ...adminAuthApi.server.tags,
       ],
       paths: {
         ...adminAuthApi.server.paths,
+        '/static/{filename}': {
+          get: {
+            tags: ['Static files'],
+            summary: 'Static files api',
+            description: 'Hit his api inorder to ensure api health.',
+            operationId: 'serveStaticFIle',
+            parameters: [
+              {
+                in: 'path',
+                name: 'filename',
+                schema: {
+                  type: 'string',
+                  enum: ['Nepal_BIM_Forum_Application.pdf', 'Nepal_BIM_Forum_Corporate.pdf']
+                },
+                description: 'filename',
+                required: true
+              }
+            ],
+            responses: {
+              default: {
+                description: '',
+                content: {
+                  'application/json': {
+                    schema: {
+                      $ref: '#/components/schemas/CommonResponse',
+                    },
+                  },
+                },
+              },
+            }
+          }
+        },
         '/health-check': {
           get: {
             tags: ['Health Check'],
