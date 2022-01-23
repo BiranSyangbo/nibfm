937817162ce500019b30e65bdd5163edd9758e6d
lekhraj.rokaya <rokayalekhraj@gmail.com>
2022-01-23 11:45:24 +0545

    get_request.detail.js file is updated

--- server/lib/modules/contact-us/controllers/get_request.detail.js
+++ server/lib/modules/contact-us/controllers/get_request.detail.js
@@ -1,8 +1,45 @@
+/**
+ * @author lekhrazz
+ * @method getContactUsRequestDetail
+ */
+
+'use strict';
+
+const { getContactUsDetail } = require('../utils/db_query.helper')
+const HTTPStatus = require('http-status');
 
 
 module.exports = async (req, res, next) => {
   try {
 
+    if (req.params.uuid) {
+
+      
+      const projection = {
+        name: 1,
+        email: 1,
+        message: 1
+      }
+      const data = await getContactUsDetail(req, req.params.uuid, projection)
+
+      if (data) {
+        return res.status(HTTPStatus.OK).json({
+          status: HTTPStatus.OK,
+          message: "Data fetched.",
+          data
+        })
+      }
+
+      return res.status(HTTPStatus.NOT_FOUND).json({
+        status: HTTPStatus.NOT_FOUND,
+        message: "Data not found."
+      })
+    }
+
+    return res.status(HTTPStatus.NOT_FOUND).json({
+      status: HTTPStatus.NOT_FOUND,
+      message: "Data not found."
+    })
   } catch (error) {
     return next(error);
   }
