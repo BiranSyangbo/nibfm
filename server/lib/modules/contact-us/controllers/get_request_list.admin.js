ea16ce5f5ac1ea996d38970cbd9bec1bf69f2f9c
lekhraj.rokaya <rokayalekhraj@gmail.com>
2022-01-23 10:38:09 +0545

    get_request_list.admin.js file is updated

--- server/lib/modules/contact-us/controllers/get_request_list.admin.js
+++ server/lib/modules/contact-us/controllers/get_request_list.admin.js
@@ -1,8 +1,60 @@
+const { getContactUsList, countTotalItems } = require('../utils/db_query.helper')
+const HTTPStatus = require('http-status');
 
+const pagerOptsHelper = (req) => {
+  try {
+    const page = req.query.page ? parseInt(req.query.page) : 1;
+    const perPage = req.query.perPage ? parseInt(req.query.perPage) : 10;
+    const offset = (page - 1) * perPage;
+    return { page, perPage, offset }
+  } catch (error) {
+    throw error;
+  }
+}
 
 module.exports = async (req, res, next) => {
   try {
+    const pagerOpts = pagerOptsHelper(req);
 
+    let queryOpts = {
+      deleted: false
+    };
+
+    if (req.query.filter && req.query.filter['name']) {
+      queryOpts.name = {
+        $regex: new RegExp('.*' + req.query.filter['name'], "i")
+      }
+    }
+
+    if (req.query.filter && req.query.filter['email']) {
+      queryOpts.email = {
+        $regex: new RegExp('.*' + req.query.filter['email'], "i")
+      }
+    }
+
+    const [dataList, count] = await Promise.all([
+      getContactUsList(req, queryOpts, pagerOpts),
+      countTotalItems(req, queryOpts)
+    ])
+
+    if (dataList && dataList.length > 0) {
+      return res.status(HTTPStatus.OK).json({
+        status: HTTPStatus.OK,
+        message: "Data fetched.",
+        dataList,
+        pagination: {
+          totalItems: count,
+          totalPages: Math.ceil(count / pagerOpts.perPage),
+          perPage: pagerOpts.perPage,
+          currentPage: pagerOpts.page
+        }
+      })
+    }
+
+    return res.status(HTTPStatus.NOT_FOUND).json({
+      status: HTTPStatus.NOT_FOUND,
+      message: "Data not found."
+    })
   } catch (error) {
     return next(error);
   }
