74f7e42509da3a1eb5f83a858150291f6ce37d55
lekhraj.rokaya <rokayalekhraj@gmail.com>
2022-03-01 23:58:21 +0545

    corporate_form_db_query.helper.js file is updated

--- server/lib/modules/membership-form/utils/corporate_form_db_query.helper.js
+++ server/lib/modules/membership-form/utils/corporate_form_db_query.helper.js
@@ -6,61 +6,62 @@ const uuid = require('uuid');
 const insert = (req, insertObj) => {
   try {
     const enterpriseSizeType = {
-      smallScale : insertObj?.enterpriseSizeType?.smallScale || false,
-      mediumScale : insertObj?.enterpriseSizeType?.mediumScale || false,
-      largeScale : insertObj?.enterpriseSizeType?.largeScale || false,
+      smallScale: insertObj?.enterpriseSizeType?.smallScale || false,
+      mediumScale: insertObj?.enterpriseSizeType?.mediumScale || false,
+      largeScale: insertObj?.enterpriseSizeType?.largeScale || false,
 
     }
 
     const membershipPeriod = {
-      annual : insertObj?.membershipPeriod?.annual || false,
-      lifetime : insertObj?.membershipPeriod?.lifetime || false,
+      annual: insertObj?.membershipPeriod?.annual || false,
+      lifetime: insertObj?.membershipPeriod?.lifetime || false,
     }
 
     const organizationType = {
-      architecturalEngineering : insertObj?.organizationalInformation?.organizationType?.architecturalEngineering || false,
-      architecturalConstruction : insertObj?.organizationalInformation?.organizationType?.architecturalConstruction || false,
-      interiorConstruction : insertObj?.organizationalInformation?.organizationType?.interiorConstruction || false,
-      interiorEngineering : insertObj?.organizationalInformation?.organizationType?.interiorEngineering || false,
-      societyConstruction : insertObj?.organizationalInformation?.organizationType?.societyConstruction || false,
-      societyEngineering : insertObj?.organizationalInformation?.organizationType?.societyEngineering || false
+      architecturalEngineering: insertObj?.organizationalInformation?.organizationType?.architecturalEngineering || false,
+      architecturalConstruction: insertObj?.organizationalInformation?.organizationType?.architecturalConstruction || false,
+      interiorConstruction: insertObj?.organizationalInformation?.organizationType?.interiorConstruction || false,
+      interiorEngineering: insertObj?.organizationalInformation?.organizationType?.interiorEngineering || false,
+      societyConstruction: insertObj?.organizationalInformation?.organizationType?.societyConstruction || false,
+      societyEngineering: insertObj?.organizationalInformation?.organizationType?.societyEngineering || false
     }
 
     const organizationHeadOfcaddress = {
-      country : insertObj?.organizationalInformation?.organizationHeadOfcaddress?.country,
-      provinceNumber : insertObj?.organizationalInformation?.organizationHeadOfcaddress?.provinceNumber,
-      district : insertObj?.organizationalInformation?.organizationHeadOfcaddress?.district,
-      tole : insertObj?.organizationalInformation?.organizationHeadOfcaddress?.tole,
-      wardNumber : insertObj?.organizationalInformation?.organizationHeadOfcaddress?.wardNumber
+      country: insertObj?.organizationalInformation?.organizationHeadOfcaddress?.country,
+      provinceNumber: insertObj?.organizationalInformation?.organizationHeadOfcaddress?.provinceNumber,
+      district: insertObj?.organizationalInformation?.organizationHeadOfcaddress?.district,
+      tole: insertObj?.organizationalInformation?.organizationHeadOfcaddress?.tole,
+      wardNumber: insertObj?.organizationalInformation?.organizationHeadOfcaddress?.wardNumber
     }
 
     const organizationalInformation = {
-      organizationName: insertObj?.organizationalInformation?.organizationName,
+      name: insertObj?.organizationalInformation?.organizationName,
       chairpersonName: insertObj?.organizationalInformation?.chairpersonName,
-      date: insertObj?.organizationalInformation?.date,
+      establishedDate: new Date(insertObj?.organizationalInformation?.date),
       email: insertObj?.organizationalInformation?.email,
       bussinessContactNumber: insertObj?.organizationalInformation?.bussinessContactNumber,
       organizationPanNumber: insertObj?.organizationalInformation?.organizationPanNumber,
       phoneNumber: insertObj?.organizationalInformation?.phoneNumber,
       organizationDesccription: insertObj?.organizationalInformation?.organizationDesccription,
       note: insertObj?.organizationalInformation?.note,
-      organizatioType : organizationType,
-      organizatioHeadOfcaddress : organizationHeadOfcaddress
+      organizatioType: organizationType,
+      organizatioHeadOfcaddress: organizationHeadOfcaddress
     }
 
     const insertObject = {
       uuid: uuid.v4(),
       corporateMembershipNumber: insertObj?.corporateMembershipNumber,
-      date: insertObj?.date,
+      date: new Date(insertObj?.date),
       enterpriseSize: insertObj?.enterpriseSize,
-      profileImage : insertObj?.profileImage,
-      enterpriseSizeType : enterpriseSizeType,
-      membershipPeriod : membershipPeriod,
-      organizationalInformation : organizationalInformation,
-      isApproved : 0,
+      profileImage: insertObj?.profileImage,
+      enterpriseSizeType: enterpriseSizeType,
+      membershipPeriod: membershipPeriod,
+      organizationalInformation,
+      isApproved: 0,
+      status: 'Pending',
       deleted: false,
       createdAt: new Date().toISOString().slice(0, 10)
-      
+
     }
     return req.db.collection(collectionName).insertOne(insertObject);
   } catch (error) {
@@ -74,57 +75,58 @@ const corporateFormUpdateStatus = (req, tableId) => {
     },
       {
         $set: {
-          isApproved: true
+          isApproved: req.body.isApproved,
+          status: req.body.isApproved === 1 ? 'Approved' : 'Rejected'
         }
       })
   } catch (error) {
     throw error;
   }
 }
-const update = (req, insertObj,tableId) => {
+const update = (req, insertObj, tableId) => {
   try {
 
     const enterpriseSizeType = {
-      smallScale : insertObj?.enterpriseSizeType?.smallScale || false,
-      mediumScale : insertObj?.enterpriseSizeType?.mediumScale || false,
-      largeScale : insertObj?.enterpriseSizeType?.largeScale || false,
+      smallScale: insertObj?.enterpriseSizeType?.smallScale || false,
+      mediumScale: insertObj?.enterpriseSizeType?.mediumScale || false,
+      largeScale: insertObj?.enterpriseSizeType?.largeScale || false,
 
     }
 
     const membershipPeriod = {
-      annual : insertObj?.membershipPeriod?.annual || false,
-      lifetime : insertObj?.membershipPeriod?.lifetime || false,
+      annual: insertObj?.membershipPeriod?.annual || false,
+      lifetime: insertObj?.membershipPeriod?.lifetime || false,
     }
 
     const organizationType = {
-      architecturalEngineering : insertObj?.organizationalInformation?.organizationType?.architecturalEngineering || false,
-      architecturalConstruction : insertObj?.organizationalInformation?.organizationType?.architecturalConstruction || false,
-      interiorConstruction : insertObj?.organizationalInformation?.organizationType?.interiorConstruction || false,
-      interiorEngineering : insertObj?.organizationalInformation?.organizationType?.interiorEngineering || false,
-      societyConstruction : insertObj?.organizationalInformation?.organizationType?.societyConstruction || false,
-      societyEngineering : insertObj?.organizationalInformation?.organizationType?.societyEngineering || false
+      architecturalEngineering: insertObj?.organizationalInformation?.organizationType?.architecturalEngineering || false,
+      architecturalConstruction: insertObj?.organizationalInformation?.organizationType?.architecturalConstruction || false,
+      interiorConstruction: insertObj?.organizationalInformation?.organizationType?.interiorConstruction || false,
+      interiorEngineering: insertObj?.organizationalInformation?.organizationType?.interiorEngineering || false,
+      societyConstruction: insertObj?.organizationalInformation?.organizationType?.societyConstruction || false,
+      societyEngineering: insertObj?.organizationalInformation?.organizationType?.societyEngineering || false
     }
 
     const organizationHeadOfcaddress = {
-      country : insertObj?.organizationalInformation?.organizationHeadOfcaddress?.country,
-      provinceNumber : insertObj?.organizationalInformation?.organizationHeadOfcaddress?.provinceNumber,
-      district : insertObj?.organizationalInformation?.organizationHeadOfcaddress?.district,
-      tole : insertObj?.organizationalInformation?.organizationHeadOfcaddress?.tole,
-      wardNumber : insertObj?.organizationalInformation?.organizationHeadOfcaddress?.wardNumber
+      country: insertObj?.organizationalInformation?.organizationHeadOfcaddress?.country,
+      provinceNumber: insertObj?.organizationalInformation?.organizationHeadOfcaddress?.provinceNumber,
+      district: insertObj?.organizationalInformation?.organizationHeadOfcaddress?.district,
+      tole: insertObj?.organizationalInformation?.organizationHeadOfcaddress?.tole,
+      wardNumber: insertObj?.organizationalInformation?.organizationHeadOfcaddress?.wardNumber
     }
 
     const organizationalInformation = {
-      organizationName: insertObj?.organizationalInformation?.organizationName,
+      name: insertObj?.organizationalInformation?.organizationName,
       chairpersonName: insertObj?.organizationalInformation?.chairpersonName,
-      date: insertObj?.organizationalInformation?.date,
+      establishedDate: new Date(insertObj?.organizationalInformation?.date),
       email: insertObj?.organizationalInformation?.email,
       bussinessContactNumber: insertObj?.organizationalInformation?.bussinessContactNumber,
       organizationPanNumber: insertObj?.organizationalInformation?.organizationPanNumber,
       phoneNumber: insertObj?.organizationalInformation?.phoneNumber,
       organizationDesccription: insertObj?.organizationalInformation?.organizationDesccription,
       note: insertObj?.organizationalInformation?.note,
-      organizatioType : organizationType,
-      organizatioHeadOfcaddress : organizationHeadOfcaddress
+      organizatioType: organizationType,
+      organizatioHeadOfcaddress: organizationHeadOfcaddress
     }
 
 
@@ -133,14 +135,14 @@ const update = (req, insertObj,tableId) => {
     },
       {
         $set: {
-      corporateMembershipNumber: insertObj?.corporateMembershipNumber,
-      date: insertObj?.date,
-      enterpriseSize: insertObj?.enterpriseSize,
-      profileImage : insertObj?.profileImage,
-      enterpriseSizeType : enterpriseSizeType,
-      membershipPeriod : membershipPeriod,
-      organizationalInformation : organizationalInformation,
-      modifiedDate : new Date().toISOString().slice(0, 10)
+          corporateMembershipNumber: insertObj?.corporateMembershipNumber,
+          // date: insertObj?.date,
+          enterpriseSize: insertObj?.enterpriseSize,
+          profileImage: insertObj?.profileImage,
+          enterpriseSizeType: enterpriseSizeType,
+          membershipPeriod: membershipPeriod,
+          organizationalInformation,
+          modifiedDate: new Date().toISOString().slice(0, 10)
         }
       })
   } catch (error) {
@@ -165,17 +167,18 @@ const deleteDocument = (req, tableId) => {
 const getList = (req, queryOpts, pagerOpts) => {
   try {
     return req.db.collection(collectionName).find(queryOpts)
-      .project({ uuid: 1, 
-                date: 1, 
-                corporateMembershipNumber: 1 ,
-                enterpriseSizeType: 1 ,
-                profileImage: 1 ,
-                membershipPeriod: 1 ,
-                organizationalInformation: 1 ,
-                isApproved: 1 ,
-                createdAt : 1
+      .project({
+        uuid: 1,
+        date: 1,
+        corporateMembershipNumber: 1,
+        enterpriseSizeType: 1,
+        profileImage: 1,
+        membershipPeriod: 1,
+        organizationalInformation: 1,
+        isApproved: 1,
+        createdAt: 1
 
-              })
+      })
       .skip(pagerOpts.offset)
       .limit(pagerOpts.perPage)
       .sort({ createdAt: -1 })
