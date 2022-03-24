029cf3833e19486d3bb10677987de42c80b5bfd3
lekhraj.rokaya <rokayalekhraj@gmail.com>
2022-03-24 21:14:42 +0545

    validation.utils.js file is updated in donation_management module

--- server/lib/modules/donation-management/utils/validation.utils.js
+++ server/lib/modules/donation-management/utils/validation.utils.js
@@ -7,8 +7,8 @@ module.exports = (reqBody) => {
     if (reqBody.date && !moment(reqBody.date, 'YYY-MM-DD', true).isValid()) return { isValid: false, msg: "Invalid date provided." };
     if (!reqBody.donerName || reqBody.donerName.trim().length === 0) return { isValid: false, msg: "Doner name is required." };
     if (!reqBody.donerCountry || reqBody.donerCountry.trim().length === 0) return { isValid: false, msg: "Doner country is required." };
-    if (!reqBody.amount || reqBody.amount.trim().length === 0) return { isValid: false, msg: "Donation amount is required." };
-    if (numberRegex.test(reqBody.amount) == false) return { isValid: false, msg: "Invalid Donation amount." };
+    if (!reqBody.amount) return { isValid: false, msg: "Donation amount is required." };
+    if (numberRegex.test(parseInt(reqBody.amount)) == false) return { isValid: false, msg: "Invalid Donation amount." };
 
     return { isValid: true, msg: "" };
   } catch (error) {
