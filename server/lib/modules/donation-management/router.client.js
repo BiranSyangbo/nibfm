849a2ef2db18e78a9009ef461a29bb4a18bbe65d
lekhraj.rokaya <rokayalekhraj@gmail.com>
2022-03-24 21:20:52 +0545

    router.client.js file is updated

--- server/lib/modules/donation-management/router.client.js
+++ server/lib/modules/donation-management/router.client.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const getDonationListForEndUser = require('./controllers/get_donation_list.client');
-const userTokenMiddleware = require('../../middleware/user_token_auth.middleware');
+// const userTokenMiddleware = require('../../middleware/user_token_auth.middleware');
 
 
-router.route('/')
-  .get(userTokenMiddleware, getDonationListForEndUser)
+router.route('/list')
+  .get(getDonationListForEndUser)
 
 module.exports = router;
