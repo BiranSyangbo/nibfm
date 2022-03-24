9c74d946b30af032c4ece73c825ea837fd9a62e2
lekhraj.rokaya <rokayalekhraj@gmail.com>
2022-03-24 20:32:43 +0545

    validation.utils.js file is created in donation-management module

---
+++ server/lib/modules/donation-management/utils/validation.utils.js
@@ -0,0 +1,17 @@
+const moment = require('moment');
+const numberRegex = new RegExp('^[0-9]*$');
+
+module.exports = (reqBody) => {
+  try {
+    if (!reqBody.date || reqBody.date.trim().length === 0) return { isValid: false, msg: "Donation date is required." };
+    if (reqBody.date && !moment(reqBody.date, 'YYY-MM-DD', true).isValid()) return { isValid: false, msg: "Invalid date provided." };
+    if (!reqBody.donerName || reqBody.donerName.trim().length === 0) return { isValid: false, msg: "Doner name is required." };
+    if (!reqBody.donerCountry || reqBody.donerCountry.trim().length === 0) return { isValid: false, msg: "Doner country is required." };
+    if (!reqBody.amount || reqBody.amount.trim().length === 0) return { isValid: false, msg: "Donation amount is required." };
+    if (numberRegex.test(reqBody.amount) == false) return { isValid: false, msg: "Invalid Donation amount." };
+
+    return { isValid: true, msg: "" };
+  } catch (error) {
+    throw error;
+  }
+}
