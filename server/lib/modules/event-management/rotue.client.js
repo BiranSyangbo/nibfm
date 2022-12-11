160f1473276d925733202ce12302a0dd153a4010
Binod Nagarkoti <binod1365@gmail.com>
2022-12-11 14:27:44 +0545

    fix event-management issues

--- server/lib/modules/event-management/rotue.client.js
+++ server/lib/modules/event-management/rotue.client.js
@@ -1,12 +1,13 @@
+/** @format */
+
 const express = require('express');
 const router = express.Router();
 
 const getEventListForClient = require('./controller/get_event_list.client');
+const getBlogDetailController = require('./controller/get_event_detail.client');
 // const userTokenMiddleware = require('../../middleware/user_token_auth.middleware');
 
+router.route('/info').get(getEventListForClient);
 
-router.route('/info')
-  .get(getEventListForClient)
-  router.route('/:slug')
-  .get(getBlogDetailController);
+router.route('/:slug').get(getBlogDetailController);
 module.exports = router;
