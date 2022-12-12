b7ced5c7f11ab52dd91bc9c47dc36e83b3ef6ab3
lekhrazz <rokayalekhraj@gmail.com>
2022-12-12 20:57:01 +0545

    modified: server/lib/modules/membership-form/controllers/submit_corporate_form.cleint.js

--- server/lib/modules/membership-form/controllers/submit_corporate_form.cleint.js
+++ server/lib/modules/membership-form/controllers/submit_corporate_form.cleint.js
@@ -17,7 +17,7 @@ const sendResetEmail = async (name) => {
 
     const message = {
       email: process.env.ADMIN_EMAIL_RECEIVE_ID,
-      title: 'Membership registration notification',
+      title: 'Corporate membership registration notification',
 
       body: `
      <div>
