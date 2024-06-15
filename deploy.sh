3f07a6aff44a62fc376a33b0be2d71a2ee10aa3c
Binod Nagarkoti <binod1365@gmail.com>
2024-06-15 21:22:43 +0545

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Change directory to where the admin application is located
+cd /home/projects/nbimf/adminApp
+
+echo "Pulling latest changes from Git..."
+git pull
+
+echo "Switching to Node.js version 10.24.1..."
+nvm use 10.24.1
+
+echo "Installing dependencies..."
+npm install
+
+echo "Building the admin application..."
+npm run build
+
+echo "Removing existing html folder..."
+rm -rf /var/www/nbimf/html
+
+echo "Copying build files to /var/www/nbimf/html..."
+scp -r ./build/* /var/www/nbimf/html
+
+echo "Reloading nginx service..."
+sudo systemctl reload nginx
+
+echo "Deployment of admin application completed successfully."
