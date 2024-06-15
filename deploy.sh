d3afb0047cc9029e36a3f7dadf9ebf2aaee676c0
Binod Nagarkoti <binod1365@gmail.com>
2024-06-15 21:42:08 +0545

    add script

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,26 @@
 #!/bin/bash
 
-# Change directory to where the admin application is located
-cd /home/projects/nbimf/adminApp
+# Change directory to where the server application is located
+cd /home/projects/nbimf/server/
 
-echo "Checking if Node.js version 10.24.1 is installed using nvm..."
-if nvm ls | grep -q "v10.24.1"; then
-  echo "Node.js version 10.24.1 is already installed."
+echo "Checking if Node.js version 16.18.0 is installed using nvm..."
+if nvm ls | grep -q "v16.18.0"; then
+  echo "Node.js version 16.18.0 is already installed."
 else
-  echo "Node.js version 10.24.1 is not installed. Installing..."
-  nvm install 10.24.1
+  echo "Node.js version 16.18.0 is not installed. Installing..."
+  nvm install 16.18.0
 fi
 
 echo "Pulling latest changes from Git..."
 git pull
 
-echo "Switching to Node.js version 10.24.1..."
-nvm use 10.24.1
+echo "Switching to Node.js version 16.18.0..."
+nvm use 16.18.0
 
 echo "Installing dependencies..."
 npm install
 
-echo "Building the admin application..."
-npm run build
+echo "Restarting server with PM2..."
+pm2 restart 0 --update-env
 
-echo "Removing all item from /var/www/nbimf/html"
-rm -rf /var/www/nbimf/html
-mkdir /var/www/nbimf/html
-
-echo "Copying build files to /var/www/nbimf/html..."
-scp -r ./build/* /var/www/nbimf/html
-
-echo "Reloading nginx service..."
-sudo systemctl reload nginx
-
-echo "Deployment of admin application completed successfully."
+echo "Deployment of server application completed successfully."
