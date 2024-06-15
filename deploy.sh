5f191e85c7b2a7402e8acaf8f4200d2e45b73c48
Binod Nagarkoti <binod1365@gmail.com>
2024-06-15 21:29:40 +0545

    add seploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # Change directory to where the admin application is located
-cd /home/projects/nbimf/admin-app
+cd /home/projects/nbimf/adminApp
+
+echo "Checking if Node.js version 10.24.1 is installed using nvm..."
+if nvm ls | grep -q "v10.24.1"; then
+  echo "Node.js version 10.24.1 is already installed."
+else
+  echo "Node.js version 10.24.1 is not installed. Installing..."
+  nvm install 10.24.1
+fi
 
 echo "Pulling latest changes from Git..."
 git pull
@@ -15,8 +23,9 @@ npm install
 echo "Building the admin application..."
 npm run build
 
-echo "Removing existing html folder..."
+echo "Removing all item from /var/www/nbimf/html"
 rm -rf /var/www/nbimf/html
+mkdir /var/www/nbimf/html
 
 echo "Copying build files to /var/www/nbimf/html..."
 scp -r ./build/* /var/www/nbimf/html
